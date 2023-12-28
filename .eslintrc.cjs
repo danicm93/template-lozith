01ad69f3de8fe00b0019a5dcc5094110b657ff11
danicm93 <danicm.web@gmail.com>
2023-12-28 14:09:39 +0100

    Add new priorities import order eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ module.exports = {
     "import/order": [
       "error",
       {
-        "groups": ["builtin", "external", "internal"],
+        "groups": ["builtin", "external", "internal", ["parent", "sibling"]],
         "pathGroups": [
           {
             "pattern": "react",
--- src/core/app.tsx
+++ src/core/app.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Home } from '@/pages'
 
 
+
 function App() {
   return (
     <Routes>
