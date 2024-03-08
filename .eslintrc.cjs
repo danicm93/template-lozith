e7acb8731a3c6a8c6a869b904a1ae5d7663e3720
danicm93 <danicm.web@gmail.com>
2024-03-08 12:22:11 +0100

    Remove core folder

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -50,6 +50,7 @@ module.exports = {
     "quotes": ["error", "single"],
     "semi": ["error","never"],
     "import/default": "off",
+    "linebreak-style": "off",
     "import/no-named-as-default-member": "off",
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-explicit-function-return-type": "off",
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+
 import { ThemeProvider } from '@emotion/react'
-import { BrowserRouter } from 'react-router-dom'
 import { CssBaseline } from '@mui/material'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-import { App, theme } from '@/core'
+import { theme } from '@/shared/styles'
 
+import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
